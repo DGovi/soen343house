dc063c364644f87c3c4bcec571ea968fe5f05265
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-20 17:49:42 -0400

    add @BeforeAll to ManageUsersTest

--- src/Tests/ManageUsersTest.java
+++ src/Tests/ManageUsersTest.java
@@ -1,39 +1,46 @@
 package Tests;
 
-import Model.Room;
-import Model.Simulation;
+import Controller.Simulation;
 import Model.User;
-import Model.UserType;
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalTime;
 import java.util.Date;
-import java.util.Timer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManageUsersTest {
 
+    private static Simulation simulation;
+    private static final File HOUSE_FILE = new File("./houseinput.json");
+
+    @BeforeAll
+    static void resetSimulation() throws IOException, JSONException {
+        // reset the simulation
+        simulation = new Simulation(new String(), java.sql.Time.valueOf(LocalTime.now()), 25, HOUSE_FILE, true);
+    }
+
     @Test
-    void addUser() throws JSONException {
+    void addUser() {
         String username = "testboy";
         String password = "123456";
         String type = "Stranger";
         String location = "Garage";
 
-        Simulation simulation = new Simulation(new Date(), java.sql.Time.valueOf(LocalTime.now()), 25, "houseinput.json");
         assertEquals("Successfully added " + username + " as a " + type.toLowerCase() + " user.", simulation.addUser(username, password, type, password));
     }
 
     @Test
-    void removeUser() throws JSONException {
+    void removeUser() {
         String username = "testboy";
         String password = "123456";
         String type = "Stranger";
         String location = "Garage";
 
-        Simulation simulation = new Simulation(new Date(), java.sql.Time.valueOf(LocalTime.now()), 25, "houseinput.json");
         simulation.addUser(username, password, type, password);
 
         User testUser = simulation.getUsers().get(1);
@@ -41,26 +48,23 @@ class ManageUsersTest {
     }
 
     @Test
-    void setUserLocation() throws JSONException {
-        Simulation simulation = new Simulation(new Date(), java.sql.Time.valueOf(LocalTime.now()), 25, "houseinput.json");
+    void setUserLocation() {
         User testUser = simulation.getUsers().get(0);
         assertEquals("Successfully made requested changes to user.", simulation.editUser(testUser.getName() +"(" + testUser.getID() + ")", testUser.getPassword(),"", "", "Kitchen"));
     }
 
     @Test
-    void setLoggedInUserLocation() throws JSONException {
-        Simulation simulation = new Simulation(new Date(), java.sql.Time.valueOf(LocalTime.now()), 25, "houseinput.json");
+    void setLoggedInUserLocation() {
         assertEquals("Successfully changed logged in users location.", simulation.setLoggedInUserLocation("Kitchen"));
     }
 
     @Test
-    void loginUser() throws JSONException {
+    void loginUser() {
         String username = "testboy";
         String password = "123456";
         String type = "Stranger";
         String location = "Garage";
 
-        Simulation simulation = new Simulation(new Date(), java.sql.Time.valueOf(LocalTime.now()), 25, "houseinput.json");
         simulation.addUser(username, password, type, password);
         assertEquals("Successfully switched users.", simulation.login(username, password));
     }
