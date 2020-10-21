eb96ab43612b41f4dfe00d462d175ef5fc8f8ca7
Alexander Shevchenko <alexander_shev@mail.ru>
2020-10-20 21:53:39 -0400

    Change test files

--- src/Tests/ModifySimulationParametersTest.java
+++ src/Tests/ModifySimulationParametersTest.java
@@ -18,40 +18,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ModifySimulationParametersTest {
     private static final File HOUSE_FILE = new File("./houseinput.json");
-    private static final File TEST_HOUSE = new File("./testInput.json");
-
-    @Test
-    void loadHouseLayout() throws IOException, JSONException {
-        Simulation simulation = new Simulation("", java.sql.Time.valueOf(LocalTime.now()), 25, TEST_HOUSE, true);
-        House loadedHouse = simulation.getHouse();
-
-        ArrayList<Room> rooms =  new ArrayList<>();
-        ArrayList<Window> windows =  new ArrayList<>();
-
-        ArrayList<String> roomsGarage = new ArrayList<>();
-        ArrayList<String> roomsKitchen = new ArrayList<>();
-
-        roomsGarage.add("Kitchen");
-        roomsKitchen.add("Garage");
-
-        windows.add(new Window());
-        rooms.add(new Room("Garage", windows, 1, roomsGarage));
-        rooms.add(new Room("Kitchen", new ArrayList<>(), 0, roomsKitchen));
-
-        House testHouse = new House(rooms,"Test location");
-
-        for(int i = 0; i < loadedHouse.getRooms().size(); i++){
-            Room rL = loadedHouse.getRooms().get(i);
-            Room rT = testHouse.getRooms().get(i);
-
-            assertEquals(rL.getName(), rT.getName());
-            assertEquals(rL.getWindows().size(), rT.getWindows().size());
-            assertEquals(rL.getLights(), rT.getLights());
-            for (int j = 0; j < rL.getDoors().size(); j++){
-                assertEquals(rL.getDoors().get(j), rT.getDoors().get(j));
-            }
-        }
-    }
 
     @Test
     void setHouseLocation() throws IOException, JSONException {
