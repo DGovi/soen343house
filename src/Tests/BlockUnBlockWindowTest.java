13b1f5af080c0e4f7510d85aacfe6804892c7dc9
Mahad Janjua <mahad.j15@hotmail.com>
2020-10-20 22:58:29 -0400

    Block/unblock window test

--- src/Tests/BlockUnBlockWindowTest.java
+++ src/Tests/BlockUnBlockWindowTest.java
@@ -15,13 +15,60 @@ class BlockUnBlockWindowTest {
     private static final File HOUSE_FILE = new File("./houseinput.json");
 
     @Test
-    void blockWindow() {
+    void blockWindow() throws IOException, JSONException {
+        Simulation simulation = new Simulation(new String(), java.sql.Time.valueOf(LocalTime.now()), 25, HOUSE_FILE, true);
+        assertEquals(
+                false,
+                simulation.getHouse().getRooms().get(0).getWindows().get(0).getOpen(),
+                "Window should be closed at the start."
+        );
+
+        simulation.getHouse().getRooms().get(0).getWindows().get(0).changeOpen();
+        simulation.getHouse().getRooms().get(0).getWindows().get(0).changeObstructed();
+
+        assertEquals(
+                true,
+                simulation.getHouse().getRooms().get(0).getWindows().get(0).getOpen(),
+                "Window should be open after running changeOpen"
+        );
+        assertEquals(
+                true,
+                simulation.getHouse().getRooms().get(0).getWindows().get(0).getObstructed(),
+                "Window should be blocked after running changeObstructed while the window is open."
+        );
 
     }
 
     @Test
-    void unBlockWindow() {
+    void unBlockWindow() throws IOException, JSONException {
+        Simulation simulation = new Simulation(new String(), java.sql.Time.valueOf(LocalTime.now()), 25, HOUSE_FILE, true);
+        assertEquals(
+                false,
+                simulation.getHouse().getRooms().get(0).getWindows().get(0).getOpen(),
+                "Window should be closed at the start."
+        );
 
+        simulation.getHouse().getRooms().get(0).getWindows().get(0).changeOpen();
+        simulation.getHouse().getRooms().get(0).getWindows().get(0).changeObstructed();
+
+        assertEquals(
+                true,
+                simulation.getHouse().getRooms().get(0).getWindows().get(0).getOpen(),
+                "Window should be open after running changeOpen"
+        );
+        assertEquals(
+                true,
+                simulation.getHouse().getRooms().get(0).getWindows().get(0).getObstructed(),
+                "Window should be blocked after running changeObstructed while the window is open."
+        );
+
+        simulation.getHouse().getRooms().get(0).getWindows().get(0).changeObstructed();
+
+        assertEquals(
+                false,
+                simulation.getHouse().getRooms().get(0).getWindows().get(0).getObstructed(),
+                "Window should be blocked after running changeObstructed again while the window is open."
+        );
     }
 
 }
