c9883b9830129e6303bc912a6e72c924b383932f
Daniel Gauvin <danielgauvin01@gmail.com>
2020-11-08 15:34:43 -0500

    added javadoc for intruder class

--- src/Model/Intruder.java
+++ src/Model/Intruder.java
@@ -1,5 +1,10 @@
 package Model;
 
+/**
+ * this class represents an intruder.
+ * An intruder's sole purpose is to enter a room and
+ * be detected by a motion sensor in a room.
+ */
 public class Intruder {
     private Room room;
 
@@ -7,6 +12,10 @@ public class Intruder {
         this.room = room;
     }
 
+    /**
+     * gets the room in the house that the intruder is in
+     * @return room object
+     */
     public Room getRoom(){
         return room;
     }
