6f9a11d9ed6cf87645f5883b9c8e5f1d7b02cf7a
Adrien Tremblay <adrien@adrientremblay.com>
2020-12-06 09:34:14 -0500

    Change SHH monitor logic to match with activity diagram

--- src/Controller/SHHMonitor.java
+++ src/Controller/SHHMonitor.java
@@ -18,8 +18,26 @@ public class SHHMonitor extends Thread{
     public void run() {
         while (true) {
             // perform monitoring
-            checkIfTempBelowZero();
-            checkIfShouldOpenWindow();
+            for (Room room : masterSim.getHouse().getRooms()) {
+                if (room.getRealTemperature() < 0) {
+                    // alert user of low temperature
+                    DashboardDriver.controllerInstance.printToConsole("ALERT: The " + room.getName() + " is at a temperature below 0!  The pipes may burst!");
+                } else {
+                    // try to open windows
+                    if (! masterSim.isSummer(Date.valueOf(masterSim.getDate()).getMonth()) || masterSim.getIsAway())
+                        return;
+
+                    if (room.getRealTemperature() < masterSim.getTemperature()) {
+                        for (Window window: room.getWindows()) {
+                            if (window.getObstructed()) {
+                                DashboardDriver.controllerInstance.printToConsole("WARNING: The SHH module tried to open a window to cool a room but it was blocked!  Cancelling operation...");
+                            } else {
+                                window.setOpen(true);
+                            }
+                        }
+                    }
+                }
+            }
 
             // sleep
             try {
@@ -28,32 +46,5 @@ public class SHHMonitor extends Thread{
                 e.printStackTrace();
             }
         }
-    }
-
-    private void checkIfShouldOpenWindow() {
-        if (! masterSim.isSummer(Date.valueOf(masterSim.getDate()).getMonth()) || masterSim.getIsAway())
-            return;
-
-        for (Room room: masterSim.getHouse().getRooms()) {
-            if (room.getRealTemperature() < masterSim.getTemperature()) {
-                for (Window window: room.getWindows()) {
-                    if (window.getObstructed()) {
-                       DashboardDriver.controllerInstance.printToConsole("WARNING: The SHH module tried to open a window to cool a room but it was blocked!  Cancelling operation...");
-                    } else {
-                        window.setOpen(true);
-                    }
-                }
-            }
-        }
-
-    }
-
-    private void checkIfTempBelowZero() {
-        for (Room room : masterSim.getHouse().getRooms()) {
-            if (room.getRealTemperature() < 0) {
-                DashboardDriver.controllerInstance.printToConsole("ALERT: The " + room.getName() + " is at a temperature below 0!  The pipes may burst!");
-            }
-        }
-
     }
 }
