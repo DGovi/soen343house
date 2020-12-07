5e5598610974a2649fd4fbc8ff456b21f49b5b85
Adrien Tremblay <adrien@adrientremblay.com>
2020-12-06 20:46:42 -0500

    BadInputException javadoc

--- src/Exceptions/BadInputException.java
+++ src/Exceptions/BadInputException.java
@@ -2,8 +2,15 @@ package Exceptions;
 
 import View.DashboardDriver;
 
+/**
+ * Exception for bad input in the UI.
+ */
 public class BadInputException extends Exception{
 
+    /**
+     * Constructor for BadInputException>
+     * @param inputType the input type that caused the exception
+     */
     public BadInputException(String inputType) {
         System.err.println("ERROR: Bad " + inputType + " input!");
     }
